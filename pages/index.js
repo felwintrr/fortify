e2ae40d3d5f8d5ccb351a8c7669b6876882a97e3
Jack Smith <felwintrr@protonmail.com>
2022-07-27 15:42:06 +0100

    feat: add bg images, change structure

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,16 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import { Box, Container, Heading, Text, Image, Flex } from "@chakra-ui/react";
 import Head from "next/head";
+import SubmitButton from "../components/connect-button";
+import Footer from "../components/footer";
 import IpfsInput from "../components/ipfs-input";
-import Arweave from "arweave";
 import Navbar from "../components/navbar";
 
 export default function Home() {
-  const arweave = Arweave.init({});
-
-  async function sendTx() {
-    const userInput = document.getElementById("userInput").value;
-    const tx = await arweave.createTransaction({ data: "hello" });
-
-    tx.addTag("IPFS-Add", { userInput });
-
-    await arweave.transactions.sign(tx);
-    response = await arweave.transactions.post(tx);
-    console.log(response.status);
-  }
 
   return (
     <Box>
       <Head>
-        <title>Fortify - Arweave NFT Uploader</title>
+        <title>Fortify - Make Your NFTs Permanent</title>
         <meta
           name="description"
           content="Permanenty switch your IPFS-stored NFT collection to Arweave."
@@ -30,18 +19,50 @@ export default function Home() {
       <Navbar />
       <Container maxW="container.xl" centerContent>
         <Container maxW="container.md">
-          <Heading fontSize="64px" fontWeight="extrabold" textAlign="center">
-            Your NFTs deserve permanent storage.
+          <Heading
+            fontSize={{ base: "3.5rem", sm: "4rem" }}
+            fontWeight="extrabold"
+            textAlign="center"
+            mt={{ base: "1rem", sm: "0rem" }}
+          >
+            Your NFTs deserve{" "}
+            <Heading
+              color="green.400"
+              fontSize={{ base: "3.5rem", sm: "4rem" }}
+              fontWeight="extrabold"
+              textAlign="center"
+              mt={{ base: "1rem", sm: "0rem" }}
+            >
+              permanent storage.
+            </Heading>
           </Heading>
-          <Text fontSize="22px" fontWeight="medium" textAlign="center" mt={6}>
+          <Text
+            fontSize="1.375rem"
+            fontWeight="medium"
+            textAlign="center"
+            mt={6}
+          >
             Get your entire collection on Arweaveⓐ with a few clicks
           </Text>
         </Container>
-        <Container mt={20} >
-          <Text fontWeight='bold' textAlign='center' p={2} >Enter your metadata CID to get started</Text>
-          <IpfsInput />
-        </Container>
       </Container>
+      <Box>
+        <Container mt={20} maxW="container.xl" centerContent>
+          <Text fontWeight="bold" textAlign="center" p={2}>
+            Enter your metadata CID to get started
+          </Text>
+          <IpfsInput />
+          <SubmitButton />
+        </Container>
+        <Flex
+          justifyContent="space-between"
+          display={{ base: "none", sm: "flex" }} /*mt="-250px"*/
+        >
+          <Image src="nfts-fortify-p-500.png" loading="lazy" opacity="0.2"/>
+          <Image src="nfts-fort-p-500.png" loading="lazy" opacity="0.2" />
+        </Flex>
+      </Box>
+      <Footer />
     </Box>
   );
 }
