2f7b305d05c717a3bce800a51f642df9d718a1c5
Jack Smith <felwintrr@protonmail.com>
2022-07-27 16:39:37 +0100

    feat(input): add responsiveness

--- components/ipfs-input.js
+++ components/ipfs-input.js
@@ -3,9 +3,9 @@ import { InputGroup, InputLeftAddon, Input, Box } from "@chakra-ui/react";
 const IpfsInput = () => {
   return (
     <Box>
-      <InputGroup size='md'>
+      <InputGroup size="md" width={{base: "300px", md:'500px'}} >
         <InputLeftAddon children="ipfs://" />
-        <Input id="userInput" type="text" placeholder="CID"/>
+        <Input id="userInput" type="text" placeholder="CID" />
       </InputGroup>
     </Box>
   );
