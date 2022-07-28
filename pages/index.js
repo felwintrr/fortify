0f080f7c2720f45cafe317b6131572c07c84b926
Jack Smith <felwintrr@protonmail.com>
2022-07-28 17:09:08 +0100

    feat: add video embed

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import IpfsInput from "../components/ipfs-input";
 import Navbar from "../components/navbar";
 import Avatars from "../components/avatars";
 import Hero from "../components/hero";
-import Background from "../components/background"
+import Background from "../components/background";
 
 export default function Home() {
   return (
@@ -29,6 +29,15 @@ export default function Home() {
         <Avatars />
       </Container>
       <Background />
+      <Container maxW="container.xl" border="8px" borderColor="gray.900" borderRadius="md" centerContent>
+        <iframe
+          width="1265"
+          height="712"
+          src="https://www.youtube.com/embed/IVzsKd2ZFs4"
+          title="Fortify YouTube video"
+          allowFullScreen
+        />
+      </Container>
       <Footer />
     </Box>
   );
