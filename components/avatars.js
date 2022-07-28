da57152a47a2caa5354e555500ab556aeef0c49f
Jack Smith <felwintrr@protonmail.com>
2022-07-28 21:25:51 +0100

    fix: change avatar names

--- components/avatars.js
+++ components/avatars.js
@@ -5,9 +5,9 @@ const Avatars = () => {
     <Container centerContent mt="3rem">
       <Flex align="center" justify="space-between">
         <AvatarGroup size="md" max={3} mr="0.5rem">
-          <Avatar name="Ryan Florence" src="running-image.png" />
-          <Avatar name="Segun Adebayo" src="moonb1.jpeg" />
-          <Avatar name="Kent Dodds" src="noun-image.png" />
+          <Avatar name="Running connection" src="running-image.png" />
+          <Avatar name="Moonbird NFT" src="moonb1.jpeg" />
+          <Avatar name="Noun NFT" src="noun-image.png" />
         </AvatarGroup>
         <Text>50,000+ NFTs have been fortified.</Text>
       </Flex>
