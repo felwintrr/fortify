b263cb8f8940546a032af6753471d38d523e47e9
Jack Smith <felwintrr@protonmail.com>
2022-07-30 10:11:41 +0100

    feat: style footer

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,33 @@
-import { Box, Container, Flex, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  HStack,
+  IconButton,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 import { FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Box p="0.5rem">
+    <Box p="1rem" mt="3rem">
       <Container maxW="container.xl">
-        <Flex justifyContent="space-between">
+        <Flex align="center" justifyContent="space-between">
           <Text>© 2022 Fortify | OnlyArweave. All rights reserved.</Text>
-          <Flex>
-            <FaTwitter />
-            <FaYoutube />
-          </Flex>
+          <HStack>
+            <Link href="https://twitter.com/onlyarweave" isExternal>
+              <FaTwitter fontSize="1.5rem" />
+            </Link>
+            <Spacer />
+            <Link
+              href="https://www.youtube.com/channel/UC-3G4h9_PkNu9pTuHb9aw3w"
+              isExternal
+            >
+              <FaYoutube fontSize="1.5rem" />
+            </Link>
+          </HStack>
         </Flex>
       </Container>
     </Box>
